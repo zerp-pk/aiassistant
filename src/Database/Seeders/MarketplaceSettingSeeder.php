7016b818fbe65980ae9e6d1d584955f923746097
HafizMMoaz <hafizmoazkhalid@gmail.com>
2026-07-02 06:18:28 +0500

    Update LandingPage reference to zerp/landing-page

--- src/Database/Seeders/MarketplaceSettingSeeder.php
+++ src/Database/Seeders/MarketplaceSettingSeeder.php
@@ -3,7 +3,7 @@
 namespace Zerp\AIAssistant\Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Workdo\LandingPage\Models\MarketplaceSetting;
+use Zerp\LandingPage\Models\MarketplaceSetting;
 use Illuminate\Support\Facades\File;
 
 class MarketplaceSettingSeeder extends Seeder
